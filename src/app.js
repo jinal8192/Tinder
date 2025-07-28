6d29a384b5ac93db9e8fc374611438956b9e31d4
jinal8192 <jinaah.jain@gmail.com>
2025-07-29 07:23:18 +0800

    exploring get,post, delete

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 const express = require("express");
 
 const app = express();
-app.use("/", (req,res) => {
-    res.send("hello from server");
+// order of the routes matter a lot
+
+app.get("/user", (req,res) => {
+    res.send({firstname: "jinal", lastname: "jain"})
 })
+
+app.post("/user", (req,res) => {
+    res.send("Data successfully saved");
+})
+
+app.delete("/user", (req,res) => {
+    res.send("Deleted successfully");
+})
+ 
+// wildcard route - match every route
+// app.use("/", (req,res) => {
+//     res.send("hello from server");
+// })
+
 app.listen(3000, () => {
     console.log("server listening on 3000 port");
 })
