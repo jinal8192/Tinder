e3db4f6f93cc2eed3a186e91803faf58abe22acc
jinal8192 <jinaah.jain@gmail.com>
2025-08-04 11:11:31 +0800

    Login Api

--- src/app.js
+++ src/app.js
@@ -34,6 +34,24 @@ app.post("/signup", async (req, res) => {
     }
 })
 
+app.post("/login", async (req, res) => {
+    try {
+        const {emailId, password} = req.body;
+        const user = await User.findOne({emailId: emailId});
+        if (!user) {
+            throw new Error("Invalid credentials");
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (isPasswordValid) {
+            res.send("Login Successfully");
+        } else {
+            throw new Error("Invalid credentials");
+        }
+    } catch (err) {
+        res.status(401).send("Login failed " + err);
+    }
+})
+
 // get user by emailId
 app.get("/user", async (req, res) => {
     const emailId = req.body.emailId;
