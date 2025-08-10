2f729abd245ce5aec44da39bc4f3d12819aa338a
jinal8192 <jinaah.jain@gmail.com>
2025-08-10 11:09:12 +0800

    coonection request model and index

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,18 +1,56 @@
 const express = require("express");
-const { userAuth } = require("../middlewares/auth")
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const router = express.Router();
 
-router.post("/sendConnectionRequest", userAuth, async (req, res) => {
-    try {
-        // const user = req.user;
-        // res.send(user);
-        console.log("sending a connection request");
-        res.send("connection established");
-
-    } catch (err) {
-        res.status(401).send("Login failed " + err);
+router.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
+  try {
+    const fromUserId = req.user._id;
+    const toUserId = req.params.toUserId;
+    const status = req.params.status;
+    const allowedStatus = ["ignored", "interested"];
+    if (!allowedStatus.includes(status)) {
+      return res.status(400).json({
+        message: "Invalid status type " + status,
+      });
     }
-})
+
+    const toUser = await User.findById(toUserId);
+    if (!toUser) {
+      return res.status(400).json({
+        message: "User not found",
+      });
+    }
+
+    // check if there is an existing connectionRequest
+    const existingConnectionRequest = await ConnectionRequest.findOne({
+      $or: [
+        { fromUserId, toUserId },
+        { fromUserId: toUserId, toUserId: fromUserId },
+      ],
+    });
+
+    if (existingConnectionRequest) {
+      return res.status(400).json({
+        message: "Connection request already exist",
+      });
+    }
+
+    const connectionRequest = new ConnectionRequest({
+      fromUserId,
+      toUserId,
+      status,
+    });
+    const data = await connectionRequest.save();
+    res.json({
+      message: req.user.firstName + " is " + status + " " + toUser.firstName,
+      data,
+    });
+  } catch (err) {
+    res.status(401).send("Connection failed " + err);
+  }
+});
 
 module.exports = router;
